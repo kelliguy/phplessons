6559d4c0924a6990236d285e586f02ca681b6e42
Kelli Guy <110922125+kelliguy@users.noreply.github.com>
2024-03-22 08:41:41 -0500

    cgvuj

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php 
+   //String
+   $name ='Coding is fun';
+   //Integer 
+   $name =15;
+   //Float
+   $name = 15.363;
+
+   //BooLean 
+
+   //Array 
+   $names = array("Kelli","Grecia","Chad");
+   echo $names['2']
+
+    ?>
+</body>
+</html>
