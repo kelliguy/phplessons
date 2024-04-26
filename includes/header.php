17632816dce4ea70ba3f0e23b96b4d1d9dae742e
kelliguy <110922125+kelliguy@users.noreply.github.com>
2024-04-26 18:28:27 -0500

    test

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start
+?>
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -42,15 +45,22 @@
 </nav>
 
 <body>
-  <?php
-  $_SESSION['username'] = "Kelli7764";
-  echo $_SESSION['username'];
-
-  if (!isset($_SESSION['username'])) {
-     echo "You are not logged in!";
-  }   else {
-    echo "You are logged in!";
+<div>
+  <?php 
+  if (isset($_SESSION['userId'])) {
+    echo '<form action="includes/logout.inc.php" method="post">
+    <button type="submit" name="logout-submit">Logout</button>
+    </form>';
   }
+   else {
+    echo ' <form action="includes/login.inc.php" method="post">
+    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+    <input type="password" name="pwd" placeholder="Password...">
+    <button type="submit" name="login-submit">Login</button>
+    </form>
+    <a href="signup.php">Signup</a>';
+   }
   ?>
-  <hr>
+
+</div>
   </body>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <body>
 <?php   
     include 'includes/header.php';
+   
     ?>
 
     <?php 
@@ -27,6 +28,17 @@
    echo $names['2']
 
     ?>
+    <main>
+<?php 
+if (isset($_SESSION['userId'])) {
+  echo '<p>You are logged in!</p>;
+}
+ else {
+  echo '<p>You are logged out!</p>;
+ }
+?>
+
+</main>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
